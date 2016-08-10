dd5c4a2876f578960731f6e3d2b8e5a893830b00
reactive-firewall <reactive-firewall@users.noreply.github.com>
2016-08-10 10:23:36 -0700

    Version 0.2.2 - More Testing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='sample',
-    version='0.2.1',
+    version='0.2.2',
     description='Fetch Public Blaklists scripts',
     long_description=readme,
     author='reactive-firewall',
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -21,6 +21,14 @@ class BasicTestSuite(unittest.TestCase):
 		"""Test IPv4 Dot notation to IP binary string"""
 		self.assertEqual(helpers.IPv4_to_IP('0.0.0.0'), '00000000000000000000000000000000')
 
+	def test_Int_to_IPv4(self):
+		"""Test Int to IPv4 Dot notation"""
+		self.assertEqual(helpers.int_to_IPv4(0), '0.0.0.0') and self.assertEqual(helpers.int_to_IPv4(111239847), '6.161.98.167') and self.assertEqual(helpers.int_to_IPv4(167239847), '9.247.224.167') and self.assertEqual(helpers.int_to_IPv4(9167239847), '136.154.62.169') and self.assertEqual(helpers.int_to_IPv4(67306243), '4.3.3.3') and self.assertEqual(helpers.int_to_IPv4(0), '0.0.0.0')
+
+	def test_IPv4_to_IP(self):
+		"""Test IPv4 Dot notation to IP binary string"""
+		self.assertEqual(helpers.IPv4_to_IP('0.0.0.0'), '00000000000000000000000000000000')
+
 
 if __name__ == '__main__':
 	unittest.main()
