e065f5a76aefa950814c870af87f969532136dd9
reactive-firewall <reactive-firewall@users.noreply.github.com>
2016-08-12 13:43:04 -0700

    Version 0.2.6 - adding more testing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='Fetch Public Blacklists',
-    version='0.2.5',
+    version='0.2.6',
     description='Fetch Public Blacklists scripts',
     long_description=readme,
     author='reactive-firewall',
