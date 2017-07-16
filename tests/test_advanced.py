4586fba977f10dd85a25919744a65ad3f433a9b1
reactive-firewall <reactive-firewall@users.noreply.github.com>
2017-07-16 02:18:14 -0700

    improved coverage a little

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -5,6 +5,68 @@ from FetchPublicBlacklists import FetchPublicBlacklists  # noqa
 from FetchPublicBlacklists import helpers  # noqa
 
 import unittest
+import subprocess
+import sys
+
+
+def getPythonCommand():
+	"""function for backend python command"""
+	thepython = "exit 1 ; #"
+	try:
+		thepython = checkPythonCommand(["which", "coverage"])
+		if (str("/coverage") in str(thepython)) and (sys.version_info >= (3, 3)):
+			thepython = str("coverage run -p")
+		elif (str("/coverage") in str(thepython)) and (sys.version_info <= (3, 2)):
+			try:
+				import coverage
+				if coverage.__name__ is not None:
+					thepython = str("{} -m coverage run -p").format(str(sys.executable))
+				else:
+					thepython = str(sys.executable)
+			except Exception:
+				thepython = str(sys.executable)
+		else:
+			thepython = str(sys.executable)
+	except Exception:
+		thepython = "exit 1 ; #"
+		try:
+			thepython = str(sys.executable)
+		except Exception:
+			thepython = "exit 1 ; #"
+	return str(thepython)
+
+
+def checkPythonCommand(args=[None], stderr=None):
+	"""function for backend subprocess check_output command"""
+	theOutput = None
+	try:
+		if args is None or args is [None]:
+			theOutput = subprocess.check_output(["exit 1 ; #"])
+		else:
+			if str("coverage ") in args[0]:
+				if sys.__name__ is None:
+					raise ImportError("Failed to import system. WTF?!!")
+				if str("{} -m coverage ").format(str(sys.executable)) in str(args[0]):
+					args[0] = str(sys.executable)
+					args.insert(1, str("-m"))
+					args.insert(2, str("coverage"))
+					args.insert(3, str("run"))
+					args.insert(4, str("-p"))
+					args.insert(4, str("--source=FetchPublicBlacklists"))
+				else:
+					args[0] = str("coverage")
+					args.insert(1, str("run"))
+					args.insert(2, str("-p"))
+					args.insert(2, str("--source=FetchPublicBlacklists"))
+			theOutput = subprocess.check_output(args, stderr=stderr)
+	except Exception as cmderror:
+		theOutput = None
+	try:
+		if isinstance(theOutput, bytes):
+			theOutput = theOutput.decode('utf8')
+	except UnicodeDecodeError:
+		theOutput = bytes(theOutput)
+	return theOutput
 
 
 class AdvancedTestSuite(unittest.TestCase):
@@ -24,11 +86,15 @@ class AdvancedTestSuite(unittest.TestCase):
 
 	def test_dryrun_config(self):
 		"""Test dry run of tool"""
-		import subprocess
 		try:
 			print(str(
-				subprocess.check_output(
-					["FetchPublicBlacklists/FetchPublicBlacklists.py", "--dry-run"]
+				checkPythonCommand(
+					[
+						getPythonCommand(),
+						"-m",
+						"FetchPublicBlacklists.FetchPublicBlacklists",
+						"--dry-run"
+					]
 				)
 			))
 		except Exception:
